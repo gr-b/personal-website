825dcdfff80bcf6ceb2567933b475a97d4134332
grbishop <grbishop@wpi.edu>
2018-04-09 11:40:29 -0400

    added UIkit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+declare var UIkit:any;
+export const uikit = UIkit;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'title changed';
+  showAlert(): void {
+    UIkit.modal.alert('UIkit alert!');
+  }
 }
