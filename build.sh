69d00c17c0a8f95a670bee38267c16a0abde983c
grbishop <grbishop@wpi.edu>
2018-04-12 15:51:54 -0400

    Deploy prod

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
 ng build --prod --base-href "https://gr-b.github.io/personal-website"
+rm -rf docs
+mv dist docs
+echo "griffinbishop.com" >> docs/CNAME
+git add --all
+git commit -m "Deploy prod"
+git push -u origin master
