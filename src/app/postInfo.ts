b9f34cc07b441ed1d28acbf76dcf04c4fef4f153
grbishop <grbishop@wpi.edu>
2018-04-17 15:40:44 -0400

    Post colors work

--- src/app/postInfo.ts
+++ src/app/postInfo.ts
@@ -1,6 +1,7 @@
 export class PostInfo {
   title: string;
   date: string;
+  color: string;
   description: string;
   image: string;
 }
--- src/app/posts.ts
+++ src/app/posts.ts
@@ -4,24 +4,28 @@ export const POSTS: PostInfo[] = [
   {
     title: 'Wayfair',
     date: 'Summer 2018',
+    color: 'green',
     description: 'Summer 2018',
     image: 'https://d2xsegqaa8s978.cloudfront.net/wayfair_0.0.4_staging/assets/logo.png'
   },
   {
     title: 'Datto',
     date: 'Summer 2017',
+    color: 'blue',
     description: 'Summer 2017',
     image: 'https://d2xsegqaa8s978.cloudfront.net/wayfair_0.0.4_staging/assets/logo.png'
   },
   {
     title: 'iD Tech',
     date: 'Summer 2016',
+    color: 'green',
     description: 'Summer 2016',
     image: 'https://d2xsegqaa8s978.cloudfront.net/wayfair_0.0.4_staging/assets/logo.png'
   },
   {
     title: 'WPI ARC',
     date: '2016-present',
+    color: 'blue',
     description: '2016-present',
     image: 'https://d2xsegqaa8s978.cloudfront.net/wayfair_0.0.4_staging/assets/logo.png'
   }
