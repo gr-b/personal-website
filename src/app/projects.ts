d4b63c08c2a10113239f4c62c7e67c793ebe3f68
gr-b <grbishop@wpi.edu>
2019-10-12 16:45:12 -0400

    Added project

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -8,9 +8,15 @@ export const PROJECTS: ProjectsInfo[] = [
     url: "https://github.com/gr-b/autoencoder-latent-space-visualization"
   },
   {
-    title: 'Deep Auto-encoder Latent Space Visualization',
+    title: 'Deep Learning for Data Privacy Classification (MQP)',
     description: '',
-    image: '/assets/autoencoder_exploration.gif',
-    url: "https://github.com/gr-b/autoencoder-latent-space-visualization"
+    image: '/assets/mqp_cover.png',
+    url: "https://web.wpi.edu/Pubs/E-project/Available/E-project-110418-210310/unrestricted/deep_learning_for_data_privacy_classification.pdf"
+  },
+  {
+    title: 'Why Uber Failed in China (Captstone)',
+    description: '',
+    image: '/assets/inquiry.png',
+    url: "/assets/inquiry.pdf"
   },
 ];
