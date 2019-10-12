59220d76610cc953e762d69b983809cf50ba96b2
gr-b <grbishop@wpi.edu>
2019-10-12 15:35:35 -0400

    Remove resume component

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -7,4 +7,10 @@ export const PROJECTS: ProjectsInfo[] = [
     image: '/assets/autoencoder_exploration.gif',
     url: "https://github.com/gr-b/autoencoder-latent-space-visualization"
   },
+  {
+    title: 'Deep Auto-encoder Latent Space Visualization',
+    description: '',
+    image: '/assets/autoencoder_exploration.gif',
+    url: "https://github.com/gr-b/autoencoder-latent-space-visualization"
+  },
 ];
