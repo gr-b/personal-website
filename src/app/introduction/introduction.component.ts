a939f9fe352f5223ef3b6778debd4285d1d0a6fb
gr-b <grbishop@wpi.edu>
2019-10-12 14:44:17 -0400

    Added email message alert

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -11,17 +11,10 @@ export class IntroductionComponent implements OnInit {
   underText: string;
 
   constructor() {
-    //--->this.imageURL = 'https://i.imgur.com/BOhQe0n.jpg';
     this.imageURL = '/assets/profile_picture.jpg';
 
-
-    //this.imageURL = 'https://i.imgur.com/LzrDFMo.jpg';
-
-    //this.imageURL = 'https://i.imgur.com/kvfNJ3O.jpg';
-    //this.imageURL = 'https://i.imgur.com/tq5xf58.jpg'; // Also good
-
     this.bigText = 'Griffin Bishop';
-    this.underText = 'Rower, Software Engineer, and Machine Learning enthusiast';
+    this.underText = 'Software Engineer, Rower, and Machine Learning enthusiast';
   }
 
   ngOnInit() {
